9ec64615b460c03c6fa27b1df7497dcfdc1a767f
Unknown <randy.paredis@student.uantwerpen.be>
2020-02-07 17:13:09 +0100

    UpdateWizard is now finished

--- main/UpdateWizard.py
+++ main/UpdateWizard.py
@@ -6,9 +6,19 @@ Author: Randy Paredis
 Date:   02/05/2020
 """
 
-from PyQt5 import QtWidgets, QtCore, uic
+from PyQt5 import QtWidgets, QtCore, uic, QtNetwork
 from main.extra.IOHandler import IOHandler
-import sys, glob, os
+from main.extra import Constants
+from urllib.request import urlretrieve
+from zipfile import ZipFile
+import sys, glob, os, json, time
+
+UPDATE_URL = "https://api.github.com/repos/RandyParedis/GraphDonkey/releases/latest"
+
+def version_lt(v1, v2):
+    M1, m1, s1 = [int(x) for x in v1.split(".")]
+    M2, m2, s2 = [int(x) for x in v2.split(".")]
+    return s1 < s2 or m1 < m2 or M1 < M2
 
 class UpdateWizard(QtWidgets.QWizard):
     class Pages:
@@ -16,22 +26,31 @@ class UpdateWizard(QtWidgets.QWizard):
         UNINSTALL = 1
         CLEAR = 2
         UPDATES = 3
-        PLUGIN = 4
 
     def __init__(self, parent = None):
         super(UpdateWizard, self).__init__(parent, QtCore.Qt.Dialog)
         uic.loadUi(IOHandler.dir_ui("UpdateWizard.ui"), self)
 
+        self.uninstallPage.isComplete = lambda: self.prog_uninstall.value() == 100
+        self.clearPage.isComplete = lambda: self.prog_clear.value() == 100
+        self.updatesPage.isComplete = lambda: self.prog_updates.value() == 100
+
+        self.nwm = QtNetwork.QNetworkAccessManager(self)
+        self.nwm.finished.connect(self.do_update)
+
         self.pb_clear.clicked.connect(self.clear)
+        self.prog_clear.valueChanged.connect(lambda _: self.clearPage.completeChanged.emit())
         self.pb_uninstall.clicked.connect(self.uninstall)
+        self.prog_uninstall.valueChanged.connect(lambda _: self.uninstallPage.completeChanged.emit())
+        self.pb_update.clicked.connect(self.updateGD)
+        self.prog_updates.valueChanged.connect(lambda _: self.updatesPage.completeChanged.emit())
+
 
     def nextId(self):
         cid = self.currentId()
         if cid == UpdateWizard.Pages.START:
             if self.rd_updates.isChecked():
                 return UpdateWizard.Pages.UPDATES
-            if self.rd_plugin.isChecked():
-                return UpdateWizard.Pages.PLUGIN
             if self.rd_uninstall.isChecked():
                 return UpdateWizard.Pages.UNINSTALL
             if self.rd_clear.isChecked():
@@ -100,6 +119,94 @@ class UpdateWizard(QtWidgets.QWizard):
             QtCore.QFile.remove(prefs.fileName())
         self.prog_clear.setValue(100)
         self.lbl_clear_info.setText("Finished")
+
+    def updateGD(self):
+        self.prog_updates.reset()
+        self.lbl_updates_info.setText("Obtaining available updates...")
+        if not (getattr(sys, 'frozen', None) and hasattr(sys, '_MEIPASS')):
+            # PREVENTS UPDATING FROM SOURCE
+            QtWidgets.QMessageBox.warning(self, "Updates Not Available",
+                                          "You cannot update GraphDonkey when running from source.\n"
+                                          "If you really want to do this, please pull the latest version of the"
+                                          " repository.")
+            self.lbl_updates_info.setText("Updates not available.")
+            return
+        self.nwm.get(QtNetwork.QNetworkRequest(QtCore.QUrl(UPDATE_URL)))
+
+    def do_update(self, reply: QtNetwork.QNetworkReply):
+        error = reply.error()
+        if error != QtNetwork.QNetworkReply.NoError:
+            QtWidgets.QMessageBox.warning(self, "Network Error", reply.errorString())
+            self.lbl_updates_info.setText("Network Error.")
+            return
+        self.lbl_updates_info.setText("Checking versions...")
+        self.prog_updates.setValue(0)
+        resp = reply.readAll().data()
+        rjson = json.loads(resp)
+        target = IOHandler.dir_root()
+        if version_lt(Constants.APP_VERSION, rjson["tag_name"][1:]):
+            assets = rjson["assets"]
+            durl = None
+            size = 0
+            name = ""
+            for asset in assets:
+                if (sys.platform == "win32" and "windows" in asset["name"]) or \
+                        (sys.platform == "linux" and "linux" in asset["name"]):
+                    durl = asset["browser_download_url"]
+                    size = asset["size"]
+                    name = asset["name"]
+                    break
+            if durl is None:
+                QtWidgets.QMessageBox.warning(self, "Update Error", "There is no new version available for your system.")
+                self.lbl_updates_info.setText("No update available.")
+                self.prog_updates.setValue(100)
+                return
+
+            # Remove all files and folders except the 'vendor' dir
+            self.lbl_updates_info.setText("Clearing existing installation...")
+            self.prog_updates.setValue(1)
+            files = QtCore.QDir(target).entryList()
+            for file in files:
+                if file in [".", "..", "vendor"]: continue
+                path = IOHandler.join(target, file)
+                if os.path.isfile(path):
+                    os.remove(path)
+                else:
+                    QtCore.QDir(path).removeRecursively()
+
+            self.lbl_updates_info.setText("Fetching archive...")
+            self.prog_updates.setValue(5)
+            time.sleep(0.01)
+            zipf = IOHandler.join(target, name)
+            def reph(bcnt, bsz, _):
+                self.prog_updates.setValue(5 + ((bcnt * bsz * 25) // size))
+            urlretrieve(durl, zipf, reph)
+
+            self.lbl_updates_info.setText("Extracting archive...")
+            self.prog_updates.setValue(30)
+            time.sleep(0.01)
+            baseName = "GraphDonkey/"
+            with ZipFile(zipf) as zf:
+                amount = len(zf.namelist())
+                i = 0
+                for member in zf.infolist():
+                    name = member.filename.replace(baseName, "", 1)
+                    if name == "": continue
+                    loc = IOHandler.join(target, name)
+                    print(loc, loc.endswith(os.sep), os.sep)
+                    if loc.endswith(os.sep):
+                        os.mkdir(loc)
+                    else:
+                        with open(loc, "wb") as outfile:
+                            outfile.write(zf.read(member))
+                    self.prog_updates.setValue(30 + ((i * 60) // amount))
+                    i += 1
+
+            self.lbl_updates_info.setText("Update complete!")
+            self.prog_updates.setValue(100)
+        else:
+            self.lbl_updates_info.setText("You already have the latest version!")
+            self.prog_updates.setValue(100)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
