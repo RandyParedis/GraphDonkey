290b21afe418469dff9e97a3b05f3e1440e13211
Randy Paredis <randy.paredis@student.uantwerpen.be>
2020-04-27 14:15:44 +0200

    Removed Trie tests

--- tests/context.py
+++ tests/context.py
@@ -11,12 +11,10 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from main.extra.IOHandler import IOHandler
-from main.editor.Intellisense import Trie
 from main import extra
 from main.wizards.UpdateWizard import version_lt
 
 # Prevent the deletion of 'unused' imports
 _ioh = IOHandler
-_trie = Trie
 _ex = extra
 _vlt = version_lt
